fc8db45cb11ee8f54be4223eab94346a92f3efaf
eremeevfd <dartfedor@mail.ru>
2017-01-09 11:41:17 +0300

    Made script.

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,46 @@
+import requests
+import whois
+import datetime
+import os
+from urllib.parse import urlparse
+import sys
+
+
 def load_urls4check(path):
-    pass
+    if os.path.exists(path):
+        with open(path) as file_with_urls:
+            return file_with_urls.read().splitlines()
+
 
 def is_server_respond_with_200(url):
-    pass
+    return requests.head(url).status_code == 200
+
+
+def get_proper_domain_name_for_whois(url):
+    return urlparse(url).hostname[5:]
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    return whois.whois(domain_name)['expiration_date'].date()
+
+
+def check_if_expires_over_month_or_more(domain_expiration_date):
+    return (((domain_expiration_date - datetime.date.today()).days) > 30)
+
+def output_sites_monitoring(urls):
+    for url in urls:
+        print(url)
+        print('Server responds with 200 code:', is_server_respond_with_200(url))
+        domain_name = get_proper_domain_name_for_whois(url)
+        domain_expiration_date = get_domain_expiration_date(domain_name)
+        print('Server expires over month:', check_if_expires_over_month_or_more(domain_expiration_date))
+        print('--------')
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 1:
+        urls = load_urls4check(input("Enter path to file with urls you wish to check: "))
+    else:
+        urls = load_urls4check(sys.argv[1])
+    output_sites_monitoring(urls)
+
+
